fa58fc732e3aa4c66aa1c3dc99c1be78e59c85bf
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2025-09-02 15:19:59 +0530

    Create Occupations.sql

---
+++ SQL/Advanced_select/Occupations.sql
@@ -0,0 +1,16 @@
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+SELECT Name || '(' || SUBSTR(Occupation,1,1) || ')'
+FROM OCCUPATIONS
+ORDER BY Name;
+
+select 'There are a total of ' || COUNT(*) || ' ' || LOWER(Occupation)||'s.'
+from occupations
+group by occupation
+order by COUNT(*), occupation;
